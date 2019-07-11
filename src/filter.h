9f7cf13e491858c0453263cdb7f2b13030d23494
bixind <vbif-98@mail.ru>
2019-07-11 16:40:22 +0200

    fixed codestyle

--- src/filter.h
+++ src/filter.h
@@ -15,47 +15,42 @@ public:
     using Node = SplittingTree::Node;
     using NodePtr = SplittingTree::NodePtr;
 
-
-    std::vector<complex_t> time;
-    std::vector<complex_t> frequency;
-    ssize_t size;
-
-    Filter(NodePtr node, ssize_t size_) : size(size_) {
+    Filter(const NodePtr& node, int64_t size) : size_(size) {
         std::vector<bool> path_mask = node->GetRootPathMask(); // optimize
-        phase.resize(static_cast<size_t>(log2(size)), 0);
+        phase_.resize(static_cast<size_t>(log2(size)), 0);
         uint64_t label = node->label;
 
         for (int i = 0; i + 1 < path_mask.size(); ++i) {
             int j = path_mask.size() - i - 1;
             if (path_mask[j]) {
-                phase[i] = std::exp(-2 * PI * I * label / (1 << (i + 1)));
+                phase_[i] = std::exp(-2 * PI * I * label / (1 << (i + 1)));
             }
         }
 
-        G[0] = 1;
-        for (int i = 0; i < phase.size(); ++i) {
-            if (phase[i] != 0) {
+        filter_[0] = 1;
+        for (int i = 0; i < phase_.size(); ++i) {
+            if (phase_[i] != 0) {
                 std::unordered_map<int, complex_t> updated_G;
-                for (auto it : G) {
+                for (auto it : filter_) {
                     int index = it.first;
-                    updated_G[index] = G[index] / 2 + phase[i] * G[(index + (size >> (i + 1))) % size] / 2;
-                    updated_G[(index + size - (size >> (i + 1))) % size] = G[(index + size - (size >> (i + 1))) % size] / 2 + phase[i] * G[index] / 2;
+                    updated_G[index] = filter_[index] / 2 + phase_[i] * filter_[(index + (size >> (i + 1))) % size] / 2;
+                    updated_G[(index + size - (size >> (i + 1))) % size] = filter_[(index + size - (size >> (i + 1))) % size] / 2 + phase_[i] * filter_[index] / 2;
                 }
-                G.swap(updated_G);
+                filter_.swap(updated_G);
                 updated_G.clear();
             }
         }
     }
 
     complex_t FilterTime(int psi) const {
-        return G[psi];
+        return filter_[psi];
     }
 
     complex_t FilterFrequency(int psi) const {
         complex_t freq =  1;
-        for (size_t i = 0; i < phase.size(); ++i) {
-            if (phase[i] != 0) {
-                freq *= (1 + phase[i] * std::exp(2 * PI * I * psi / (1 << (i + 1))));
+        for (size_t i = 0; i < phase_.size(); ++i) {
+            if (phase_[i] != 0) {
+                freq *= (1 + phase_[i] * std::exp(2 * PI * I * psi / (1 << (i + 1))));
             }
         }
         return freq;
@@ -63,6 +58,7 @@ public:
 
 private:
 
-    std::vector<complex_t> phase;
-    std::unordered_map<int, complex_t> G;
+    int64_t size_;
+    std::vector<complex_t> phase_;
+    std::unordered_map<int, complex_t> filter_;
 };
