2ad513265051d09d615f707132df26a132929fac
bixind <vbif-98@mail.ru>
2019-09-10 13:41:12 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-implementation of dimension independent sparse fourier transform
+Implementation of dimension independent sparse fourier transform
+# Requirements
+To compile and run tests you will need:
+- CMAKE >=3.8
+- FFTW3
+
+To make plots you will additionally need:
+- python 3.5
+- matplotlib
+
+# Compilation
+Open repository directory in terminal and run:
+```
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+```
+
+Now you can run one of the `test_*` executables in `build` directory.
+
+# Making graphs
+
+After compilation, you can use `run_experiment.py` to make graphs. You can use calls in `plot_experiments.sh`
+as examples.
+
+Run `plot_experiments.sh` to plot several basic graphs.
--- plot_experiments.sh
+++ plot_experiments.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --zero_test_coef=0.03 --output=random_with_comb.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=27 --use_comb_filter=1 --extra_data=./measures.txt --output=random_with_comb.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=27 --use_comb_filter=0 --output=random_without_comb.png
+#python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --zero_test_coef=0.03 --output=random_with_comb.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=27 --use_comb_filter=1 --extra_data=./measures.txt --output=random_with_comb.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=27 --use_comb_filter=0 --output=random_without_comb.png
 
-python3 experiment_comb.py --path=./build/measure_run --signal_type=comb --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --output=comb_with_comb.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=comb --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=0 --output=comb_without_comb.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=comb --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --output=comb_with_comb.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=comb --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=0 --output=comb_without_comb.png
 
-python3 experiment_comb.py --path=./build/measure_run --signal_type=combined --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --output=combined_with_comb.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=combined --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=0 --output=combined_without_comb.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=combined --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --output=combined_with_comb.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=combined --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=0 --output=combined_without_comb.png
 
-python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=1 --first_logn=6 --last_logn=21 --sparsity=32 --use_comb_filter=1 --output=1d_random.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=2 --first_logn=4 --last_logn=10 --sparsity=32 --use_comb_filter=1 --output=2d_random.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --output=3d_random.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=4 --first_logn=2 --last_logn=5 --sparsity=32 --use_comb_filter=1 --output=4d_random.png
-python3 experiment_comb.py --path=./build/measure_run --signal_type=random --dimensions=5 --first_logn=2 --last_logn=4 --sparsity=32 --use_comb_filter=1 --output=5d_random.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=1 --first_logn=6 --last_logn=21 --sparsity=32 --use_comb_filter=1 --output=1d_random.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=2 --first_logn=4 --last_logn=10 --sparsity=32 --use_comb_filter=1 --output=2d_random.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=3 --first_logn=3 --last_logn=7 --sparsity=32 --use_comb_filter=1 --output=3d_random.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=4 --first_logn=2 --last_logn=5 --sparsity=32 --use_comb_filter=1 --output=4d_random.png
+python3 run_experiment.py --path=./build/measure_run --signal_type=random --dimensions=5 --first_logn=2 --last_logn=4 --sparsity=32 --use_comb_filter=1 --output=5d_random.png
